c7ce8bda6e36c382876435745889bcdd6f715b84
Blaser993 <corradodepinto.design@gmail.com>
2023-09-07 15:19:43 +0200

    funzione di stampa arrey

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+//FUNZIONI
+
+void StampaArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write(array[i] + ", ");
+        }
+        else
+        {
+            Console.Write(array[i]);
+        }
+    }
+    Console.WriteLine("]");
+}
