07b75f6044d3a9ea3dc8f6379797cc54fe0058ae
Blaser993 <corradodepinto.design@gmail.com>
2023-09-07 16:03:59 +0200

    arrayquadro

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -11,6 +11,15 @@ int quadrato = Quadrato(numero);
 Console.WriteLine($"Il quadrato di {numero} è {quadrato}");
 
 
+int[] array1 = { 2, 3, 4, 5 };
+
+int[] arrayQuadro = ElevaArrayAlQuadrato(array1);
+
+Console.Write($"dato questo array1: ");
+StampaArray(array1);
+Console.Write($"moltiplico al quadrato ogni suo elemento ed ottngo il seguente array: ");
+StampaArray(arrayQuadro);
+
 
 //FUNZIONI
 
@@ -33,7 +42,18 @@ void StampaArray(int[] array)
 
 int Quadrato(int numero)
 {
-    return numero * numero;
+    return numero * numero; 
+}
 
-    
+int[] ElevaArrayAlQuadrato(int[] array)
+{
+    int[] arrayCopy = (int[])array.Clone();
+
+    for (int i = 0; i < arrayCopy.Length; i++)
+    {
+        arrayCopy[i] = Quadrato(arrayCopy[i]) ;
+    }
+
+    return arrayCopy;  
+
 }
